ee17e6d6f18bc0cfeea606ea7ee73ae540632262
clx1608@qq.com <hbdennischen@outlook.com>
2020-01-16 21:17:53 +0800

    static web

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
-const logger = require('@finelets/hyper-rest/app/Logger')
-logger.info("hello world");
+const logger = require('@finelets/hyper-rest/app/Logger'),
+    appBuilder = require('@finelets/hyper-rest/express/AppBuilder').begin(__dirname)
 
+appBuilder
+    .setWebRoot('/fuze/root', './client')
+    .setFavicon('client/imgs/favicon.jpg')
+    .end();
+
+let server = appBuilder.run(() => {
+    const addr = server.address();
+    logger.info('the server is running and listening at ' + addr.port);
+});
